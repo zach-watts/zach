28e8be136aba173c4e1f8018cabca099f65e2fdd
zach-watts <111317246+zach-watts@users.noreply.github.com>
2023-03-16 21:59:32 +0000

    Thu, Mar 16, 2023, 2:59 PM -07:00

--- js/index.js
+++ js/index.js
@@ -3,3 +3,7 @@ const button = document.querySelector('.btn2')
 const button = document.querySelector('.btn3')
 const button = document.querySelector('.btn4')
 const button = document.querySelector('.btn5')
+
+button.addEventListener('click', () => {
+    
+})
