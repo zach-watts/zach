10a5664d924962b9aa25d5ce66bb996a41defe14
zach-watts <111317246+zach-watts@users.noreply.github.com>
2023-03-27 14:40:20 -0700

    SOME MESSAGE

--- js/progress.js
+++ js/progress.js
@@ -1,4 +1,4 @@
-const progress = document.getElemmentById('progress')
+const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
@@ -18,37 +18,36 @@ next.addEventListener('click', () => {
 })
 
 
-prev.addEventlistener('click', () => {
+prev.addEventListener('click', () => {
     currentActive--
 
     if (currentActive < 1) {
         currentActive = 1
     }
-
     update()
 })
 
 function update() {
-   progress.style.width = (currentActive - 1) / 3 * 100 + '%'
+    progress.style.width = (currentActive - 1) / 3 * 100 + '%'
 
-   if (currentActive == 1) {
+    if (currentActive == 1) {
         prev.disabled = true
-   }
-   else if (currentActive == 4){
+    }
+    else if (currentActive == 4) {
         next.disabled = true
-   }
-   else {
-    prev.disabled = false
-    next.disabled = false
-   }
+    }
+    else {
+        prev.disabled = false
+        next.disabled = false
+    }
 
 
-   circles.forEach((circle, index) => {
-        if ( index <= currentActive) {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
             circle.classList.add('active')
         }
         else {
             circle.classList.remove('active')
         }
-   })
+    })
 }
